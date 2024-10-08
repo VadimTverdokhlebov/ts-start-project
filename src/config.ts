a935d5907329483808ef329c4030b61c5fc5e0b9
VadimTverdokhlebov <vadtverdohlebov@mail.ru>
2024-10-08 12:24:04 +0300

    init

--- src/config.ts
+++ src/config.ts
@@ -3,29 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfig {
-    databaseHost: string;
-    databasePort: number;
-    databaseUser: string;
-    databasePassword: string;
-    databaseName: string;
-    serverPort: number;
-    serverHost: string;
-    user: {
-        secretKey: string;
-    };
+    APP_URL: string;
+    LOGIN: string;
+    PASSWORD: string;
 }
 
 const config: IConfig = {
-    databaseHost: process.env.DATABASE_HOST!,
-    databasePort: parseInt(process.env.DATABASE_PORT!, 10),
-    databaseUser: process.env.DATABASE_USER!,
-    databasePassword: process.env.DATABASE_PASSWORD!,
-    databaseName: process.env.DATABASE_NAME!,
-    serverPort: parseInt(process.env.SERVER_PORT!, 10),
-    serverHost: process.env.SERVER_HOST!,
-    user: {
-        secretKey: process.env.SECRET_KEY!
-    }
+    APP_URL: process.env.APP_URL!,
+    LOGIN: process.env.LOGIN!,
+    PASSWORD: process.env.PASSWORD!,
 };
 
 export default config;
